d4cece8fd59dddad736805a2a101c0c7b16fafda
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-06-29 09:57:50 -0400

    Added imported css files to

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './css/App.css'
 import {Routes,Route} from "react-router-dom"
 import Home from './pages/home'
 import Favorites from './pages/favorite'
--- front end/src/pages/home.jsx
+++ front end/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import MovieCard from "../components/moviecard"
 import { useState } from "react";
+import "../css/Home.css"
 
 
 function Home(){
