7dc4eaccd2c62c3f10350c02fc23138749a7f6ce
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-06-28 21:35:43 -0400

    React Movie Site

---
+++ front end/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieCard from './components/moviecard'
+
+function App() {
+  return (
+  <>
+  <MovieCard movie={{title: "Tim's Film", release}}
+   </>
+  )
+}
+
+
+export default App
