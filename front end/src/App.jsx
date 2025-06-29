3634ad4075c19da8337b9c3c4d694f5034e01d0a
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-06-29 09:40:28 -0400

    Added 'Routes' and Favorites Page

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-//import MovieCard from './components/moviecard'
+import {Routes,Route} from "react-router-dom"
 import Home from './pages/home'
+import Favorites from './pages/favorite'
 
 function App() {
   return (
-  <>
-  <Home />
-   </>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/favorites' element={<Favorites/>}></Route>
+
+      </Routes>
+    </main>
   )
 }
 
--- front end/src/pages/home.jsx
+++ front end/src/pages/home.jsx
@@ -1,6 +1,10 @@
 import MovieCard from "../components/moviecard"
+import { useState } from "react";
+
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+    
     const movies = [
         {id: 1, title: "nice", release_date: "2021"},
         {id: 2, title: "nice wow", release_date: "2022"},
@@ -8,11 +12,27 @@ function Home(){
 
     ];
 
+    const handleSearch = () => {
+        e.preventDefault() //stops the page from refreshing
+        alert(searchQuery)
+    };
+
     return (
     <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" 
+            placeholder="Search for movies..." 
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)} //how to update the state from an input element
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
         <div className="movies-grid">
             {movies.map((movie) => (
-            <MovieCard movie={movie} key={movie.id}/> 
+            //movie.title.toLowerCase().startsWith(searchQuery) && (This would be used without an api)
+            (<MovieCard movie={movie} key={movie.id}/>)
             ))}
         </div>
     </div>
